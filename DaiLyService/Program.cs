a500f1ce2556a439b9e2503da96bbf2ec5365714
Thuan <thuantoppo@gmail.com>
2026-03-15 21:09:24 +0700

    feat: cấu hình DI cho tồn kho service

--- DaiLyService/Program.cs
+++ DaiLyService/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddScoped<IDaiLyRepository, DaiLyRepository>();
 builder.Services.AddScoped<IDaiLyService, DaiLyService.Services.DaiLyService>();
 builder.Services.AddScoped<IKhoRepository, KhoRepository>();
 builder.Services.AddScoped<IKhoService, KhoService>();
+builder.Services.AddScoped<ITonKhoRepository, TonKhoRepository>();
+builder.Services.AddScoped<ITonKhoService, TonKhoService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
