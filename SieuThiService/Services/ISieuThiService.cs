2e2afc7447f94a3019fcc3e3b7e17b3f60dda65b
Thuan <thuantoppo@gmail.com>
2026-03-21 10:22:51 +0700

    feat: Implement Service layer cho SieuThi business logic

--- SieuThiService/Services/ISieuThiService.cs
+++ SieuThiService/Services/ISieuThiService.cs
@@ -1,7 +1,16 @@
+using SieuThiService.Models.DTOs;
+
 namespace SieuThiService.Services
 {
     public interface ISieuThiService
     {
-        // TODO: Define SieuThi service methods
+        List<SieuThiDTO> GetAll();
+        SieuThiDTO? GetById(int maSieuThi);
+        SieuThiDTO? GetByTaiKhoan(int maTaiKhoan);
+        bool Create(SieuThiCreateDTO sieuThiDto);
+        bool Update(int maSieuThi, SieuThiUpdateDTO sieuThiDto);
+        bool Delete(int maSieuThi);
+        bool ExistsByTenDangNhap(string tenDangNhap);
+        bool ExistsByEmail(string email);
     }
 }
