8e0a12cc6d3ea79b4da6922e459f533fe306f98d
Thuan <thuantoppo@gmail.com>
2026-03-23 20:39:44 +0700

    feat: add VanChuyen service layer for business logic

--- DaiLyService/Services/IVanChuyenService.cs
+++ DaiLyService/Services/IVanChuyenService.cs
@@ -1,7 +1,16 @@
+using DaiLyService.Models.DTOs;
+
 namespace DaiLyService.Services
 {
     public interface IVanChuyenService
     {
-        // TODO: Define VanChuyen service methods
+        List<VanChuyenDTO> GetAll();
+        List<VanChuyenDTO> GetByTrangThai(string trangThai);
+        List<VanChuyenDTO> GetByLo(int maLo);
+        VanChuyenDTO? GetById(int maVanChuyen);
+        int Create(VanChuyenCreateDTO dto);
+        bool UpdateTrangThai(int maVanChuyen, string trangThai, DateTime? ngayKetThuc = null);
+        bool Delete(int maVanChuyen);
+        int CountByTrangThai(string trangThai);
     }
 }
--- DaiLyService/Services/VanChuyenService.cs
+++ DaiLyService/Services/VanChuyenService.cs
@@ -1,7 +1,32 @@
+using DaiLyService.Data;
+using DaiLyService.Models.DTOs;
+
 namespace DaiLyService.Services
 {
     public class VanChuyenService : IVanChuyenService
     {
-        // TODO: Implement VanChuyen service methods
+        private readonly IVanChuyenRepository _repo;
+
+        public VanChuyenService(IVanChuyenRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<VanChuyenDTO> GetAll() => _repo.GetAll();
+
+        public List<VanChuyenDTO> GetByTrangThai(string trangThai) => _repo.GetByTrangThai(trangThai);
+
+        public List<VanChuyenDTO> GetByLo(int maLo) => _repo.GetByLo(maLo);
+
+        public VanChuyenDTO? GetById(int maVanChuyen) => _repo.GetById(maVanChuyen);
+
+        public int Create(VanChuyenCreateDTO dto) => _repo.Create(dto);
+
+        public bool UpdateTrangThai(int maVanChuyen, string trangThai, DateTime? ngayKetThuc = null) => 
+            _repo.UpdateTrangThai(maVanChuyen, trangThai, ngayKetThuc);
+
+        public bool Delete(int maVanChuyen) => _repo.Delete(maVanChuyen);
+
+        public int CountByTrangThai(string trangThai) => _repo.CountByTrangThai(trangThai);
     }
 }
