36bf0a4830f49dc7c53c650aa27d8beb5c52f701
Thuan <thuantoppo@gmail.com>
2026-03-23 09:06:34 +0700

    feat: Cấu hình dependency injection cho TonKho SieuThiService

--- SieuThiService/Program.cs
+++ SieuThiService/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddScoped<ISieuThiService, SieuThiBusinessService>();
 builder.Services.AddScoped<IKhoRepository, KhoRepository>();
 builder.Services.AddScoped<IKhoService, KhoService>();
 
+// Register TonKho dependencies for SieuThi
+builder.Services.AddScoped<ITonKhoRepository, TonKhoRepository>();
+builder.Services.AddScoped<ITonKhoService, TonKhoService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
