969318cc0d2b0bcb5d2d4d06cc659fada8b3b546
Thuan <thuantoppo@gmail.com>
2026-03-24 10:41:41 +0700

    feat: thêm service layer cho API kiểm định business logic

--- DaiLyService/Services/IKiemDinhService.cs
+++ DaiLyService/Services/IKiemDinhService.cs
@@ -1,7 +1,16 @@
+using DaiLyService.Models.DTOs;
+
 namespace DaiLyService.Services
 {
     public interface IKiemDinhService
     {
-        // TODO: Define KiemDinh service methods
+        List<KiemDinhDTO> GetAll();
+        List<KiemDinhDTO> GetByLo(int maLo);
+        List<KiemDinhDTO> GetByKetQua(string ketQua);
+        KiemDinhDTO? GetById(int maKiemDinh);
+        int Create(KiemDinhCreateDTO dto);
+        bool Update(int maKiemDinh, KiemDinhUpdateDTO dto);
+        bool Delete(int maKiemDinh);
+        int CountByKetQua(string ketQua);
     }
 }
--- DaiLyService/Services/KiemDinhService.cs
+++ DaiLyService/Services/KiemDinhService.cs
@@ -1,7 +1,31 @@
+using DaiLyService.Data;
+using DaiLyService.Models.DTOs;
+
 namespace DaiLyService.Services
 {
     public class KiemDinhService : IKiemDinhService
     {
-        // TODO: Implement KiemDinh service methods
+        private readonly IKiemDinhRepository _repo;
+
+        public KiemDinhService(IKiemDinhRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<KiemDinhDTO> GetAll() => _repo.GetAll();
+
+        public List<KiemDinhDTO> GetByLo(int maLo) => _repo.GetByLo(maLo);
+
+        public List<KiemDinhDTO> GetByKetQua(string ketQua) => _repo.GetByKetQua(ketQua);
+
+        public KiemDinhDTO? GetById(int maKiemDinh) => _repo.GetById(maKiemDinh);
+
+        public int Create(KiemDinhCreateDTO dto) => _repo.Create(dto);
+
+        public bool Update(int maKiemDinh, KiemDinhUpdateDTO dto) => _repo.Update(maKiemDinh, dto);
+
+        public bool Delete(int maKiemDinh) => _repo.Delete(maKiemDinh);
+
+        public int CountByKetQua(string ketQua) => _repo.CountByKetQua(ketQua);
     }
 }
